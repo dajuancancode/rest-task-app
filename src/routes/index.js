0372fa4c067822cafcb70a9112542f8af8e9a25d
DaJuan Harris <dajuancancode@gmail.com>
2019-07-06 18:47:14 -0400

    Route Fiexes for heroku

--- src/routes/index.js
+++ src/routes/index.js
@@ -23,25 +23,25 @@ const upload = multer({
 
 
 
-router.post('/users/signup', users.createUser)
-router.post('/users/login', users.loginUser)
-router.post('/users/logout', authMiddleware, users.logoutUser)
-router.post('/users/logoutAll', authMiddleware, users.logoutAll)
+router.post('users/signup', users.createUser)
+router.post('users/login', users.loginUser)
+router.post('users/logout', authMiddleware, users.logoutUser)
+router.post('users/logoutAll', authMiddleware, users.logoutAll)
 
-router.get('/users/me', authMiddleware, users.userProfile)
-router.patch('/users/me', authMiddleware, users.updateUser)
-router.delete('/users/me', authMiddleware, users.removeUser)
-router.post('/users/me/avatar', authMiddleware, upload.single('avatar'), users.uploadAvatar, (error, req, res, next) => {
+router.get('users/me', authMiddleware, users.userProfile)
+router.patch('users/me', authMiddleware, users.updateUser)
+router.delete('users/me', authMiddleware, users.removeUser)
+router.post('users/me/avatar', authMiddleware, upload.single('avatar'), users.uploadAvatar, (error, req, res, next) => {
   res.status(400).send({error: error.message})
 })
-router.delete('/users/me/avatar', authMiddleware, users.deleteAvatar)
-router.get('/users/:id/avatar', users.fetchAvatar)
+router.delete('users/me/avatar', authMiddleware, users.deleteAvatar)
+router.get('users/:id/avatar', users.fetchAvatar)
 
-router.get('/tasks', authMiddleware, tasks.listTasks)
-router.post('/tasks/create', authMiddleware, tasks.createTask)
+router.get('tasks', authMiddleware, tasks.listTasks)
+router.post('tasks/create', authMiddleware, tasks.createTask)
 
-router.get('/task/:id', authMiddleware, tasks.readTask)
-router.patch('/task/:id', authMiddleware, tasks.updateTask)
-router.delete('/task/:id', authMiddleware, tasks.removeTask)
+router.get('task/:id', authMiddleware, tasks.readTask)
+router.patch('task/:id', authMiddleware, tasks.updateTask)
+router.delete('task/:id', authMiddleware, tasks.removeTask)
 
 module.exports = router
